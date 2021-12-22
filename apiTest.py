f593e95e60a18ef7fcb38f313b21b4a0618799ce
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2021-12-21 20:14:07 -0500

    Add apiTest.py

---
+++ apiTest.py
@@ -0,0 +1,27 @@
+import vt
+from datetime import datetime, timedelta
+import time
+
+client = vt.Client("<insertAPIKeyHere")
+
+urls = ['www.google.com', 'www.facebook.com', 'www.twitter.com', 'www.amazon.com']
+currentTime = datetime.now()
+
+for url in urls:
+    url_id = vt.url_id(url)
+    urlResult = client.get_object("/urls/{}", url_id)
+    if urlResult.last_analysis_date <= (currentTime - timedelta(days = 7)):
+        analysis = client.scan_url(url)
+        notCompleted = True
+        while notCompleted:
+            analysis = client.get_object('/analyses/{}', analysis.id)
+            if analysis.status == 'completed':
+                print()
+                notCompleted = False
+            else:
+                time.sleep(30)
+        print(analysis.stats)
+    else:
+        print(url, ': Already scanned')
+client.close()
+print('Done')
