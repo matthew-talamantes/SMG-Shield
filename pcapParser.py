1693df54326213066686da31ced9cae31b80b73f
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-01-04 21:18:13 -0500

    Filter on http requests only

--- pcapParser.py
+++ pcapParser.py
@@ -3,5 +3,7 @@ import pyshark
 # The "display_filter" parameter allows you to use the same display filters as wireshark to filter the search.
 cap = pyshark.FileCapture('testPcap.pcap', display_filter="http")
 for packet in cap:
-    print(packet.number)
+    # Check for host so as not to double count by seeing the responses
+    if hasattr(packet.http, 'host'):
+        print(packet.http.host)
 cap.close()
