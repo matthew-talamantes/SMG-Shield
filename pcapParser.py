f788b2c9bb80f426cc6a68a9dd42fc6558343cda
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-01-06 19:11:22 -0500

    Create UrlDict from pcap

--- pcapParser.py
+++ pcapParser.py
@@ -1,9 +1,22 @@
 import pyshark
 
-# The "display_filter" parameter allows you to use the same display filters as wireshark to filter the search.
-cap = pyshark.FileCapture('testPcap.pcap', display_filter="http")
-for packet in cap:
-    # Check for host so as not to double count by seeing the responses
-    if hasattr(packet.http, 'host'):
-        print(packet.http.host)
-cap.close()
+def getUrlDict(pcapFile: str):
+    # The "display_filter" parameter allows you to use the same display filters as wireshark to filter the search.
+    # Pull URLs
+    cap = pyshark.FileCapture(pcapFile, display_filter="http")
+
+    urlDict = {}
+    for packet in cap:
+        # Check for host so as not to double count by seeing the responses
+        if hasattr(packet.http, 'host'):
+            if packet.http.host in urlDict:
+                urlDict[packet.http.host] += 1
+            else:
+                urlDict[packet.http.host] = 1
+
+    cap.close()
+    return urlDict
+
+
+urlDict = getUrlDict('testPcap.pcap')
+print(urlDict)
