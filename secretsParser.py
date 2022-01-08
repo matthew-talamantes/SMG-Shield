f9fe09289c1cc2e3144fd214856cfdf169bf7397
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-01-08 10:01:16 -0500

    Add scretsParser.py

---
+++ secretsParser.py
@@ -0,0 +1,18 @@
+def getKeyValue(item):
+    item = item.strip()
+    itemList = item.split(':')
+    return itemList
+
+def getKey(keyName):
+    with open('./secrets.txt', 'r') as file:
+        secretsDict = {}
+        for line in file.readlines():
+            lineList = getKeyValue(line)
+            secretsDict[lineList[0]] = lineList[1]
+    
+    return secretsDict[keyName]
+
+
+
+if __name__ == '__main__':
+    print(getKey('virustotal'))
