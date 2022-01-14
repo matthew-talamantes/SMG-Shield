c0f292ef326cf163b911337b941f9c17d2e4898c
Matthew Talamantes <38108928+matthew-talamantes@users.noreply.github.com>
2022-01-13 21:54:37 -0500

    Thursday progress

--- apiCallFrame.py
+++ apiCallFrame.py
@@ -4,6 +4,8 @@ import time
 import requests
 import json
 
+from fileReader import read_value
+
 def ipApi(ip, vtKey):
     # Returns the last_analysis_stats of the given IP.
     # TO-DO: handle non 200 responses
@@ -14,6 +16,8 @@ def ipApi(ip, vtKey):
     return resStats
 
 def apicall(vtkey, urlDict, ipList=None):
+    apiCalls = 0
+    apiMaxCalls = 250
     urllist = []
     results= {'urls': {}, 'ips': {}}
     for key in urlDict.keys():
@@ -23,38 +27,55 @@ def apicall(vtkey, urlDict, ipList=None):
 
     urls = urllist
     currentTime = datetime.now()
+    cachedUrls = read_value('results.csv', 'urls')
 
     for url in urls:
-        url_id = vt.url_id(url)
-        try:
-            urlResult = client.get_object("/urls/{}", url_id) 
-        except:
-            print(f'No record for: {url} found. Continuing...')
-        
-        else:
-            if urlResult.last_analysis_date <= (currentTime - timedelta(days = 7)):
-                print('Unscanned addresses found. Scanning Now...')
-                analysis = client.scan_url(url)
-                notCompleted = True
-                while notCompleted:
-                    analysis = client.get_object('/analyses/{}', analysis.id)
-                    if analysis.status == 'completed':
-                        print()
-                        notCompleted = False
+        if url not in cachedUrls:
+            if apiCalls <= apiMaxCalls:
+                url_id = vt.url_id(url)
+                try:
+                    urlResult = client.get_object("/urls/{}", url_id) 
+                except:
+                    print(f'No record for: {url} found. Continuing...')
+                
+                else:
+                    if urlResult.last_analysis_date <= (currentTime - timedelta(days = 7)):
+                        print('Unscanned addresses found. Scanning Now...')
+                        analysis = client.scan_url(url)
+                        notCompleted = True
+                        while notCompleted:
+                            analysis = client.get_object('/analyses/{}', analysis.id)
+                            if analysis.status == 'completed':
+                                print()
+                                notCompleted = False
+                            else:
+                                time.sleep(30)
+                        # results.append(analysis.stats)
+                        results['urls'][url] = analysis.stats
                     else:
-                        time.sleep(30)
-                # results.append(analysis.stats)
-                results['urls'][url] = analysis.stats
+                        # print(f'{url}: harmless: {urlResult.last_analysis_stats["harmless"]}, malicious: {urlResult.last_analysis_stats["malicious"]}')
+                        results['urls'][url] = urlResult.last_analysis_stats
+                    apiCalls += 1
             else:
-                # print(f'{url}: harmless: {urlResult.last_analysis_stats["harmless"]}, malicious: {urlResult.last_analysis_stats["malicious"]}')
-                results['urls'][url] = urlResult.last_analysis_stats
+                print('ERROR: Max API calls reached!')
+        else:
+            results['urls'][url] = cachedUrls[url]
 
     # Get IP scores
+    cachedIps = read_value('results.csv', 'ips')
     for ip in ipList:
-        ipStats = ipApi(ip, vtkey)
-        results['ips'][ip] = ipStats
+        if ip not in cachedIps:
+            if apiCalls <= apiMaxCalls:
+                ipStats = ipApi(ip, vtkey)
+                results['ips'][ip] = ipStats
+                apiCalls += 1
+            else:
+                print('ERROR: Max API calls reached!')
+        else:
+            results['ips'][ip] = cachedIps[ip]
         
     client.close()
+    print(f'{apiCalls} API calls made.')
     
     return results
 
--- fileReader.py
+++ fileReader.py
@@ -1,22 +1,6 @@
 from datetime import datetime, timedelta
+import os
 
-#Write file function
-
-def write_value(results_dict, file_name):
-
-#Take the dict of URLs and IP's and it'll take file name
-#Open the file that is passed through
-    current_time = datetime.now()
-    file = open(file_name, "w")
-    file.write("URL/IP, type, timestamp, harmless, malicious, suspicious, undetected, timeout\n")
-#loop through the URLs and IPs
-    for key, value in results_dict.items():
-#For each URL/IP, we'll write to the file the URL/IP, the time and their stats. 
-        for item, stats in value.items():
-            file.write(f"{item}, {key}, {current_time}, {stats['harmless']}, {stats['malicious']}, {stats['suspicious']}, {stats['undetected']}, {stats['timeout']}\n")
-#Close file
-    file.close()
-    print("Message")
 
 def get_line_list(line):
     linelist = line.split(",")
@@ -24,9 +8,21 @@ def get_line_list(line):
         linelist[pos] = item.strip()
     return linelist
 
+def getDateFromStr(dateStr):
+    year = int(dateStr[0:4:])
+    month = int(dateStr[5:7:])
+    day = int(dateStr[8:10:])
+    hour = int(dateStr[11:13:])
+    minute = int(dateStr[14:16:])
+    second = int(dateStr[17:19:])
+    millisecond = int(dateStr[20::])
+    date = datetime(year, month, day, hour, minute, second, millisecond)
+    return date
+
+
 
 #read file function
-def read_value(file_name):
+def read_value(file_name, type):
     current_time = datetime.now()
 #open file
     file = open(file_name, "r")
@@ -36,11 +32,35 @@ def read_value(file_name):
 #and the count as the value
     for line in file:
         linelist = get_line_list(line)
-        timestamp = datetime(linelist[2])
-        if linelist[2] >= (current_time - timedelta(days=7)):
-            results_dict[linelist[1]] [linelist[0]] = {"harmless": int(linelist[3]), "malicious": int(linelist[4]), "suspicious": int(linelist[5]), "undetected": int(linelist[6]), "timeout": int(linelist[7])}
+        timestamp = getDateFromStr(linelist[2])
+        if timestamp >= (current_time - timedelta(days=7)) and linelist[1] == type:
+            results_dict[linelist[0]] = {"harmless": int(linelist[3]), "malicious": int(linelist[4]), "suspicious": int(linelist[5]), "undetected": int(linelist[6]), "timeout": int(linelist[7])}
     file.close()
     return results_dict
 #build a results dict
 
 #return results dict
+
+
+#Write file function
+
+def write_value(results_dict, file_name):
+    
+    existingUrls = read_value(file_name, 'urls')
+    existingIps = read_value(file_name, 'ips')
+#Take the dict of URLs and IP's and it'll take file name
+#Open the file that is passed through
+    current_time = datetime.now()
+    fileExists = os.path.isfile(file_name)
+    file = open(file_name, "a")
+    if not fileExists:
+        file.write("URL/IP, type, timestamp, harmless, malicious, suspicious, undetected, timeout\n")
+#loop through the URLs and IPs
+    for key, value in results_dict.items():
+#For each URL/IP, we'll write to the file the URL/IP, the time and their stats. 
+        for item, stats in value.items():
+            if item not in existingUrls and item not in existingIps:
+                file.write(f"{item}, {key}, {current_time}, {stats['harmless']}, {stats['malicious']}, {stats['suspicious']}, {stats['undetected']}, {stats['timeout']}\n")
+#Close file
+    file.close()
+    print("Message")
